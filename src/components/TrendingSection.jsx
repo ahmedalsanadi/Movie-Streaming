5ea5d468587b3c10b005d13db89db20a2fad23ae
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-16 23:14:36 +0300

    Define options for trendingType toggle switch

--- src/components/TrendingSection.jsx
+++ src/components/TrendingSection.jsx
@@ -8,6 +8,13 @@ import HorizontalSlider from './HorizontalSlider';
 const TrendingSection = () => {
     const [trendingType, setTrendingType] = useState('day');
     const { movies, loading, error } = useTrendingMovies(trendingType);
+    const trendingOptions = [
+        { value: 'day', label: 'Today' },
+        { value: 'week', label: 'This Week' }
+      ];
+    
+      if (error) return <p>{error}</p>;
+    
   return (
     <div>TrendingSection</div>
   )
