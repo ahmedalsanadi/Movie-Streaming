67eeb2172190d1c53148e0136072b08df9778833
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-16 23:09:39 +0300

    feat : Initial Setup -Import Hooks and Components

---
+++ src/components/TrendingSection.jsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from 'react';
+import { useTrendingMovies } from '../hooks/useTrendingMovies';
+import ToggleSwitch from './ToggleSwitch';
+import MediaCard from './MediaCard'; 
+import HorizontalSlider from './HorizontalSlider';
+
+const TrendingSection = () => {
+  return (
+    <div>TrendingSection</div>
+  )
+}
+
+export default TrendingSection
