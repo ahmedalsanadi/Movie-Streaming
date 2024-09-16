92ac79d73860299d3e3a62638ff61a301b5d448d
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-16 23:24:38 +0300

    Implement ToggleSwitch component for day/week selection
    The ToggleSwitch component to allow users to toggle between "Today" and "This Week" options.

--- src/components/ToggleSwitch .jsx
+++ src/components/ToggleSwitch .jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const ToggleSwitch  = () => {
+const ToggleSwitch = ({ options, selectedOption, onChange }) => {
   return (
-    <div>ToggleSwitch </div>
-  )
-}
+    <div className='toggle-switch inline-flex items-center gap-4'>
+      <div className="bg-gray-300 rounded-full flex items-center">
+        {options.map((option) => (
+          <button
+            key={option.value}
+            className={`py-1 px-4 rounded-full transition-all duration-700 ${
+              selectedOption === option.value
+                ? 'bg-[#032541] text-white'
+                : 'text-gray-600'
+            }`}
+            onClick={() => onChange(option.value)}
+          >
+            {option.label}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default ToggleSwitch 
+export default ToggleSwitch;
