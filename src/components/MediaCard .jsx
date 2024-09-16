cba3641eb689f9223b6f4e5b1281c23eca1350bf
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-16 23:30:01 +0300

    feat : Implement MediaCard component for displaying movie information
       -Create the MediaCard component to display each movie's information,
        including an image, title, vote average, and release date. It includes
        a circular progress bar for the vote average.
       - style the circular progress in globals.css

--- src/components/MediaCard .jsx
+++ src/components/MediaCard .jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-const MediaCard  = () => {
+const MediaCard = ({ imageUrl, title, voteAverage, releaseDate, link }) => {
   return (
-    <div>MediaCard </div>
-  )
-}
+    <Link href={link}>
+      <div className="inline-flex flex-col justify-evenly px-1 rounded-b-2xl py-2 mr-4 w-44 transform transition-transform hover:scale-105 hover:shadow-2xl h-full cursor-pointer -z-10">
+        <div className="relative">
+          <Image
+            src={imageUrl}
+            alt={title}
+            width={150}
+            height={225}
+            className="mb-2 rounded-2xl w-full -z-10"
+          />
 
-export default MediaCard 
+          {/* Circular Progress Bar */}
+          {voteAverage && (
+            <div className="absolute bottom-2 left-2 z-30">
+              <div
+                className="circle-progress"
+                role="progressbar"
+                style={{ '--progress': voteAverage * 10 }}
+              >
+                <div className="inner">
+                  {Math.round(voteAverage * 10)}
+                  <span>x</span>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="px-4">
+          <h3 className="text-lg block font-semibold mt-2 dark:text-white text-left break-words whitespace-normal">
+            {title}
+          </h3>
+          <p className="text-gray-400 text-left block text-lg font-thin">
+            {releaseDate
+              ? new Date(releaseDate).toLocaleDateString('en-US', {
+                  month: 'short',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              : 'Unknown Release Date'}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default MediaCard;
