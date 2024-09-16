48a9a09bce7ad2c1406d89f4a3b91a14342b2d8f
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-16 23:18:52 +0300

    feat :Create base layout for TrendingSection with container and toggle switch
    - Add loading state handling for TrendingSection :
        Add a loading state to display a loading message when movies are being fetched.

--- src/components/TrendingSection.jsx
+++ src/components/TrendingSection.jsx
@@ -15,9 +15,37 @@ const TrendingSection = () => {
     
       if (error) return <p>{error}</p>;
     
-  return (
-    <div>TrendingSection</div>
-  )
+      return (
+        <div className='container mx-auto font-custom px-4 '>
+          <div className="trending-section">
+            <div className="toggle-switch flex items-center gap-4 mb-6">
+              <h2 className="text-2xl font-bold pl-4">Trending</h2>
+    
+              {/* Toggle Switch */}
+              <ToggleSwitch
+                options={trendingOptions}
+                selectedOption={trendingType}
+                onChange={setTrendingType}
+              />
+            </div>
+                    {/* Loading State */}
+        {loading ? <p>Loading...</p> : (
+          <HorizontalSlider>
+            {movies.map((movie) => (
+              <MediaCard
+                key={movie.id}
+                imageUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                title={movie.title}
+                voteAverage={movie.vote_average}
+                releaseDate={movie.release_date}
+                link={`/movie/${movie.id}`}  // Dynamic link for each movie
+              />
+            ))}
+          </HorizontalSlider>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default TrendingSection
