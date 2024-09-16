4792820e2861e8b3df46024de6df8caaf91594ca
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-16 23:11:53 +0300

    Add trendingType state and hook to fetch trending movies
    mplement useState to manage the trending type (day or week) and fetch trending movies based on the selection.

--- src/components/TrendingSection.jsx
+++ src/components/TrendingSection.jsx
@@ -6,6 +6,8 @@ import MediaCard from './MediaCard';
 import HorizontalSlider from './HorizontalSlider';
 
 const TrendingSection = () => {
+    const [trendingType, setTrendingType] = useState('day');
+    const { movies, loading, error } = useTrendingMovies(trendingType);
   return (
     <div>TrendingSection</div>
   )
