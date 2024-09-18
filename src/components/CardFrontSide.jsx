53f0f5ce4e133fb61e18a4cb92a9021d57dad132
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-19 01:50:14 +0300

    feat : Implement CardFrontSide Component to be re-usable
    - Created the `CardFrontSide` component to display a(movie , tvShow) card with front and back sides.
    - Designed the front side of the card to showcase the (movie , tvShow) poster with a title overlay:
      - Utilized the `Image` component from Next.js for optimized image loading.
      - Implemented a fallback for missing poster images with a gray placeholder.
    - Added a black translucent overlay at the bottom for better title visibility.
    - Designed the back side of the card to display additional (movie , tvShow) information using the `CardBackSide` component.
    - Incorporated smooth transition effects for flipping the card, enhancing user interaction.
    - Ensured responsive design with a maximum width for optimal display on various devices.
    
    This commit effectively captures the functionality and design of the `CardFrontSide` component!

---
+++ src/components/CardFrontSide.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import CardBackSide from "./CardBackSide";
+const CardFrontSide = ({ data , imgBaseUrl }) => {
+    // Constants
+
+    return (
+      <div className="relative w-full max-w-xs h-[350px] mx-auto perspective">
+        {/* Front side of the card */}
+        <div className="card-front absolute w-full h-full bg-opacity-20 shadow-md rounded-lg overflow-hidden transform transition-transform duration-500">
+          {data.poster_path ? (
+            <Image
+              src={`${imgBaseUrl}${data.poster_path}`}
+              alt={data.title}
+              width={250}
+              height={200}
+              className="w-full h-full object-cover rounded-t-lg"
+            />
+          ) : (
+            <div className="w-full h-full bg-gray-100"></div>
+          )}
+          <div className="absolute bottom-0 w-full bg-black bg-opacity-80 text-white text-center p-2 text-lg font-semibold font-custom">
+            {data.title}
+          </div>
+        </div>
+  
+        {/* Back side of the card */}
+        <div className="card-back absolute w-full h-full bg-gradient-to-br from-[#022c43] to-[#0a3548] text-white flex items-center justify-center rounded-lg transform rotate-y-180 transition-transform duration-500 shadow-xl">
+          <CardBackSide data={data} />
+        </div>
+      </div>
+    );
+  };
+  export default CardFrontSide;
+  
--- src/components/CategoryMovies.jsx
+++ src/components/CategoryMovies.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import CardFrontSide from "./CardFrontSide";
 
-// Constants
 const imgBaseUrl = "https://image.tmdb.org/t/p/w500";
 
 // Component to display the list of movies in a category
@@ -25,7 +25,7 @@ const CategoryMovies = ({ movies, categoryId }) => {
         <div className="col-span-12 lg:col-span-9">
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {movies?.length > 0 ? (
-              movies.map((movie) => <CardFrontSide key={movie.id} movie={movie} />)
+              movies.map((movie) => <CardFrontSide key={movie.id} data={movie} imgBaseUrl={imgBaseUrl} />)
             ) : (
               <p className="col-span-full text-center text-gray-500">
                 No movies found
@@ -41,59 +41,8 @@ const CategoryMovies = ({ movies, categoryId }) => {
 export default CategoryMovies;
 
 // Component to render a movie card with front and back views
-const CardFrontSide = ({ movie }) => {
-  return (
-    <div className="relative w-full max-w-xs h-[350px] mx-auto perspective">
-      {/* Front side of the card */}
-      <div className="card-front absolute w-full h-full bg-opacity-20 shadow-md rounded-lg overflow-hidden transform transition-transform duration-500">
-        {movie.poster_path ? (
-          <Image
-            src={`${imgBaseUrl}${movie.poster_path}`}
-            alt={movie.title}
-            width={250}
-            height={200}
-            className="w-full h-full object-cover rounded-t-lg"
-          />
-        ) : (
-          <div className="w-full h-full bg-gray-100"></div>
-        )}
-        <div className="absolute bottom-0 w-full bg-black bg-opacity-80 text-white text-center p-2 text-lg font-semibold font-custom">
-          {movie.title}
-        </div>
-      </div>
-
-      {/* Back side of the card */}
-      <div className="card-back absolute w-full h-full bg-gradient-to-br from-[#022c43] to-[#0a3548] text-white flex items-center justify-center rounded-lg transform rotate-y-180 transition-transform duration-500 shadow-xl">
-        <CardBackSide movie={movie} />
-      </div>
-    </div>
-  );
-};
 
 // Component to display movie details on the back side of the card
-const CardBackSide = ({ movie }) => {
-  return (
-    <div className="p-6 text-left">
-      {/* Movie Details Title */}
-      <h2 className="text-3xl font-bold mb-3 text-white">Details</h2>
-
-      {/* Movie Overview */}
-      <p className="text-base mb-2 text-gray-400">{movie.overview.slice(0, 100)}</p>
-
-      {/* Release Date Section */}
-      <div className="mb-2">
-        <p className="font-semibold text-yellow-300">Release Date:</p>
-        <p className="text-base text-gray-400 italic">{movie.release_date}</p>
-      </div>
-
-      {/* Rating Section */}
-      <div className="mb-4">
-        <p className="font-semibold text-yellow-300">Rating:</p>
-        <StarRating rating={movie.vote_average} />
-      </div>
-    </div>
-  );
-};
 
 // Component to display star rating
 const StarRating = ({ rating }) => {
