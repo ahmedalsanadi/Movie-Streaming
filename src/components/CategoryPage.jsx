7397796b89b422d7e9f316a627234636d691b7f3
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-21 02:24:11 +0300

    feat: add dark mode support to CategoryPage component
    - Updated the category title text color to adapt to dark mode using 'dark:text-white'
    - Removed unnecessary background color for the filter options section
    - Minor cleanups to the filter options text styling

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -1,30 +1,27 @@
-
-import React from "react"
-import CardFrontSide from "./CardFrontSide"
-const imgBaseUrl = "https://image.tmdb.org/t/p/w500"
+import React from "react";
+import CardFrontSide from "./CardFrontSide";
+const imgBaseUrl = "https://image.tmdb.org/t/p/w500";
 
 // Helper function to capitalize the first letter of a string
 const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
-}
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
 
 // Component to display the list of movies and tv shows in a category page
 const CategoryPage = ({ pageTitle, shows, categoryId }) => {
   return (
-    <div className="max-w-7xl mx-auto px-4 py-8">
+    <div className="max-w-7xl mx-auto px-4 py-8 text-[#032541] dark:text-white">
       {/* Category Title */}
-      <h1 className="text-3xl font-bold mb-8 text-center text-[#032541]">
+      <h1 className="text-3xl font-bold mb-8 text-center">
         {pageTitle === "tv" ? "TV Shows" : "Movies"} in{" "}
         {capitalizeFirstLetter(categoryId.replace(/-/g, " "))} Category
       </h1>
 
       <div className="grid grid-cols-12 gap-4">
         {/* Filter Options Section */}
-        <div className="col-span-12 lg:col-span-3 bg-gray-100">
+        <div className="col-span-12 lg:col-span-3 ">
           <div className="filter-options">
-            <p className="text-center text-black">
-              Filter Options (Coming Soon)
-            </p>
+            <p className="text-center">Filter Options (Coming Soon)</p>
           </div>
         </div>
 
@@ -49,8 +46,7 @@ const CategoryPage = ({ pageTitle, shows, categoryId }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryPage
-
+export default CategoryPage;
