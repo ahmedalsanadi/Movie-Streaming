1269929f2b2e17879ae30ce094fcc52a49c7ce51
Ahmed Al-Dhayani <bplahmed28@gmail.com>
2024-09-21 00:25:16 +0300

    chore: fiex some issuse in darkmode and delete some styles in horizontalsider and alt issues in image in forntendside

--- src/components/HorizontalSlider.jsx
+++ src/components/HorizontalSlider.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import bgImg from '../images/trending-bg.svg';
+import React from "react"
+import bgImg from "../images/trending-bg.svg"
 
 const HorizontalSlider = ({ children }) => {
   return (
-    <div className="mx-auto overflow-x-scroll px-4 py-1 overflow-y-hidden scrollbar-hide scroll-smooth scrollbar-thin scrollbar-thumb-yellow-300 dark:scrollbar-thumb-yellow-200 dark:scrollbar-track-[#032541] 
-    scrollbar-thumb-rounded bg-bottom bg-no-repeat " style={{ width: '100%', whiteSpace: 'nowrap', backgroundImage: `url(${bgImg.src})`, backgroundPosition: '50% 250px;' }}>
+    <div
+      className="mx-auto px-4 py-1 bg-bottom bg-no-repeat "
+      style={{
+        width: "100%",
+        whiteSpace: "nowrap",
+        backgroundImage: `url(${bgImg.src})`,
+        backgroundPosition: "50% 250px;",
+      }}
+    >
       {children}
     </div>
-  );
-};
+  )
+}
 
-export default HorizontalSlider;
+export default HorizontalSlider
--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,23 +1,40 @@
 import { useState, useEffect } from "react"
 
 export const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useState(false)
-
-  useEffect(() => {
-    // Check if we're in a browser environment
+  const [darkMode, setDarkMode] = useState(() => {
     if (typeof window !== "undefined") {
       const prefersDarkScheme = window.matchMedia(
         "(prefers-color-scheme: dark)",
       ).matches
-
       const savedMode = localStorage.getItem("darkMode")
-      setDarkMode(savedMode ? savedMode === "true" : prefersDarkScheme)
+      return savedMode !== null ? JSON.parse(savedMode) : prefersDarkScheme
+    }
+    return false // Default to false in SSR
+  })
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Listen for changes in system dark mode preference
+      const darkModeMediaQuery = window.matchMedia(
+        "(prefers-color-scheme: dark)",
+      )
+
+      const handleChange = (e) => {
+        if (!localStorage.getItem("darkMode")) {
+          setDarkMode(e.matches) // Update if there's no saved preference
+        }
+      }
+
+      darkModeMediaQuery.addEventListener("change", handleChange)
+
+      return () =>
+        darkModeMediaQuery.removeEventListener("change", handleChange)
     }
   }, [])
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      localStorage.setItem("darkMode", darkMode)
+      localStorage.setItem("darkMode", JSON.stringify(darkMode))
       document.documentElement.classList.toggle("dark", darkMode)
     }
   }, [darkMode])
