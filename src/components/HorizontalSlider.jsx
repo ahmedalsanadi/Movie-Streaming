5291c54f54d6fa316ab8c78af6ef3af406c75de5
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-23 14:27:51 +0300

    fix : Background Img size  (contain)

--- src/components/HorizontalSlider.jsx
+++ src/components/HorizontalSlider.jsx
@@ -5,13 +5,13 @@ const HorizontalSlider = ({ children }) => {
   return (
     <div
       className="mx-auto overflow-x-scroll px-4 py-1 overflow-y-hidden scrollbar-hide scroll-smooth scrollbar-thin scrollbar-thumb-yellow-300 dark:scrollbar-thumb-yellow-200 dark:scrollbar-track-[#032541] 
-      scrollbar-thumb-rounded bg-bottom bg-no-repeat h-[460px]"
+      scrollbar-thumb-rounded bg-bottom bg-no-repeat h-[460px] sm:bg-transparent md:bg-contain  lg:bg-contain"
       style={{
         width: '100%',
         whiteSpace: 'nowrap',
         backgroundImage: `url(${bgImg.src})`,
         backgroundPosition: 'center bottom',
-        backgroundSize: 'contain', // Ensure the background fits well
+      
       }}
     >
       {children}
