ee562700d96343b6fae07bcc774436c2e2da8613
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-23 21:34:16 +0300

    feat: add scrollar color for dark and light mode

--- src/components/HorizontalSlider.jsx
+++ src/components/HorizontalSlider.jsx
@@ -4,7 +4,7 @@ import bgImg from '../images/trending-bg.svg';
 const HorizontalSlider = ({ children }) => {
   return (
     <div
-      className="mx-auto overflow-x-scroll px-4 py-1 overflow-y-hidden scrollbar-hide scroll-smooth scrollbar-thin scrollbar-thumb-yellow-300 dark:scrollbar-thumb-yellow-200 dark:scrollbar-track-[#032541] 
+      className="mx-auto overflow-x-scroll px-4 py-1 overflow-y-hidden scrollbar-hide scroll-smooth scrollbar-thin scrollbar-thumb-yellow-300  dark:scrollbar-thumb-yellow-200 dark:scrollbar-track-gray-900  
       scrollbar-thumb-rounded bg-bottom bg-no-repeat h-[460px] sm:bg-transparent md:bg-contain  lg:bg-contain"
       style={{
         width: '100%',
