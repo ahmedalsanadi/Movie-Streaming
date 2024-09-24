74877943320cfd83d2e70b193d931ed5083ad5c4
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-24 21:05:06 +0300

    feat : Added TrendingUp and Search Icons for trending header and its items

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 // components/SearchBar.js
 import { useState, useEffect } from 'react';
-import { SearchIcon, XIcon } from '@heroicons/react/solid';
+import { TrendingUpIcon, SearchIcon, XIcon } from '@heroicons/react/solid';
 import { fetchDataFromTMDB } from '../util/fetchDataFromTMDB';
 
 const SearchBar = ({ isOpen, onClose }) => {
@@ -33,7 +33,8 @@ const SearchBar = ({ isOpen, onClose }) => {
   return (
     <div className="absolute top-16 left-0 w-full bg-white dark:bg-gray-800 shadow-md">
       <div className="container mx-auto px-4 py-4">
-     
+
+     {/* searchBar input starts here ------- */}
         <form onSubmit={handleSearch} className="relative mt-6 md:mt-2">
           <input
             type="text"
@@ -47,17 +48,24 @@ const SearchBar = ({ isOpen, onClose }) => {
           </button>
         </form>
         
+      {/* Trending list */}
         <div className="mt-4">
-          <h3 className="text-lg font-semibold mb-2 text-gray-700 dark:text-gray-300">Trending</h3>
+          <div className="flex items-center space-x-2 mb-2 ">
+            <TrendingUpIcon className="w-5 h-5  text-gray-500 dark:text-gray-400 " />
+            <h3 className="text-lg font-semibold  text-gray-700 dark:text-gray-300">Trending</h3>
+          </div>
           <ul className="space-y-2">
             {trendingSearches.map((item) => (
-              <li key={item.id} className="text-sm text-gray-600 dark:text-gray-400 hover:text-[#01b4e4] cursor-pointer">
+              <li key={item.id} className="flex items-center text-sm text-gray-600 dark:text-gray-400 hover:text-[#01b4e4] cursor-pointer border-b border-gray-400 border-opacity-35 p-1">
+                <SearchIcon className="w-4 h-4 mr-2 text-gray-500 dark:text-gray-400" />
                 {item.title || item.name}
               </li>
             ))}
           </ul>
         </div>
       </div>
+      
+      {/* close icon starts here ---*/}
       <button onClick={onClose} className="absolute top-1 right-2">
         <XIcon className="w-6 h-6 text-gray-500 dark:text-gray-400" />
       </button>
